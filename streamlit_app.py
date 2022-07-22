e0eb9640cf99d4b9e329bfc9e900c941b38d3450
17Sruthi <78374070+17Sruthi@users.noreply.github.com>
2022-07-22 13:32:55 +0530

    Update streamlit_app.py
    naming error fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if streamlit.button('Get Fruit Load List'):
   
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
-  with my_cnx_cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values('from streamlit')")
     return "Thanks for adding " + new_fruit
 
@@ -76,4 +76,4 @@ if streamlit.button('Add a Fruit to the List'):
   streamlit.text(back_from_function)
 
   # don't run anything past here while we troubleshoot
-# streamlit.stop()
+streamlit.stop()
